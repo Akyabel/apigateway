5a68ae27115735565a80a2dae24369d52146642f
Sebastian Rojas <serogr99@gmail.com>
2025-08-01 21:46:58 -0600

    Se agregaron los métodos GET y POST

--- src/main/java/com/srg/app/apigateway/controller/UserController.java
+++ src/main/java/com/srg/app/apigateway/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.srg.app.apigateway.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,6 +38,11 @@ public class UserController {
     }
 
     @PostMapping("")
-    public String 
+    public ResponseEntity<Object> createUser(@RequestBody User user){
+
+        User createdUser = userService.createUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                            .body("User created with id: " + createdUser.getId());
+    } 
 
 }
