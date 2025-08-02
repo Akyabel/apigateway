00390cf177a713c3f3296063cf511dc07b8fe78e
Sebastian Rojas <serogr99@gmail.com>
2025-08-01 21:48:10 -0600

    Se agregó el servicio para crear un usuario

--- src/main/java/com/srg/app/apigateway/service/UserService.java
+++ src/main/java/com/srg/app/apigateway/service/UserService.java
@@ -1,5 +1,22 @@
 package com.srg.app.apigateway.service;
 
+import org.springframework.stereotype.Service;
+
+import com.srg.app.apigateway.model.User;
+import com.srg.app.apigateway.repository.UserRepository;
+
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    public User createUser(User user){
+        return userRepository.save(user);
+    }
+
 
 }
