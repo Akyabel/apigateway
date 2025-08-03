a25fba590a9dbefe5fd26af60f3551877b62d71a
Sebastian Rojas <serogr99@gmail.com>
2025-08-03 13:33:07 -0600

    Se agrego el método para eliminar usuarios mediante id en el UserController

--- src/main/java/com/srg/app/apigateway/controller/UserController.java
+++ src/main/java/com/srg/app/apigateway/controller/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -45,4 +46,8 @@ public class UserController {
                             .body("User created with id: " + createdUser.getId());
     } 
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteUser(@PathVariable Long id){
+       userRepository.deleteById(id);
+       return ResponseEntity.ok().body("User deleted successfully with id: " + id); 
 }
