ba9172a4a86b1a1c9a8f951207cdf359ebccd5f4
Sebastian Rojas <serogr99@gmail.com>
2025-08-03 20:47:38 -0600

    Se agregó el método para listar todos los usuarios

--- src/main/java/com/srg/app/apigateway/controller/UserController.java
+++ src/main/java/com/srg/app/apigateway/controller/UserController.java
@@ -1,8 +1,9 @@
 package com.srg.app.apigateway.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +11,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.srg.app.apigateway.model.User;
@@ -28,6 +28,13 @@ public class UserController {
 
     public UserController(UserService userService){
         this.userService = userService;
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<User>> getAllUsers(){
+        List<User> users = userRepository.findAll();
+        
+        return ResponseEntity.ok().body(users);
     }
 
     @GetMapping("/{id}")
